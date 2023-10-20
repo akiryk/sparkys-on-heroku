55df565ea75db500621d7aadb449634dabd506a6
adam kiryk <akiryk@wayfair.com>
2023-10-20 19:51:38 -0400

    yes

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -6,8 +6,8 @@ export const { getClient } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: "https://first-next-with-mongodb-bv9zh15jv-akiryk.vercel.app//api/graphql",
-      // uri: "http://localhost:3000/api/graphql",
+      // uri: "https://first-next-with-mongodb-bv9zh15jv-akiryk.vercel.app//api/graphql",
+      uri: "http://localhost:3000/api/graphql",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       fetchOptions: { cache: "no-store" },
